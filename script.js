6929c29e0900272dbe11f5d84d4bc0264d3957d8
Lindsay Nunez <lindsayanunez@gmail.com>
2018-04-04 22:08:13 -0400

    Added addTodo function.

--- script.js
+++ script.js
@@ -1,20 +1,38 @@
 var todoList = {
 
 //list variable
-
+todos:[],
 
 //display
+displayTodos: function(){
+
+},
 
 //add
+addTodo: function(todo){
+  this.todos.push(todo{
+    todoText = todoText,
+    completed = false
+  });
+},
 
 //change
+changeTodo: function(){
+
+},
 
 //delete
+deleteTodo: function(){
 
+},
 //toggle complete
+toggleComplete: function(){
+
+},
 
 //toggle all
+toggleAll: function(){
 
-
+},
 
 }
