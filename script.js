7c37adbbaed0ba4b9f1cdfb8438473df6f29f931
Lindsay Nunez <lindsayanunez@gmail.com>
2018-04-06 13:54:28 -0400

    Added event listeners to the buttons on click.

--- script.js
+++ script.js
@@ -72,7 +72,16 @@ toggleAll: function(){
       }
       },
 
+var displayTodosButton = document.getElementById('displayTodosButton');
+var toggleAllButton = document.getElementById('toggleAllButton');
 
+displayTodosButton.addEventListener('click', function(){
+  todoList.displayTodos();
+});
+
+toggleAllButton.addEventListener('click', function(){
+  todoList.toggleAll();
+});
 
 
 
